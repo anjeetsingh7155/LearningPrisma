dca6a18f29b217e0b0db518805ce09487f818874
anjeetsingh7155 <anjeetsingh7155>
2026-07-01 10:47:55 +0530

    Removed the Prisma to install it freshely because of too many errors are comming

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Mere Chodu log")
+import express, { type Request, type Response } from 'express';
+
+const DB_Url : string| undefined =process.env.DATABASE
+
+
+ 
+
+
+async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+  // prisma.
+}
+const app = express();
+app.use(express.json());
+
+
+const port = 5000
+
+app.post('/signup', (req: Request, res: Response) => {
+  const {username , password ,firstName , lastName}    = req.body
+   // prisma.create
+  
+})
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is Running on http://localhost:${port}`)
+})
